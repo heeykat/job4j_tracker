f75c76b6b2653344abcc848ce4f6dcb04c48c558
heeykat <heeykat@yandex.ru>
2021-02-12 15:12:27 +0300

    1.8. Состояние объекта [#438126]

---
+++ src/test/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,25 @@
+
+package ru.job4j.oop;
+
+public class Battery {
+
+    private int load;
+
+    public Battery(int size) {
+        this.load = size;
+    }
+
+    public void exchange(Battery another){
+        another.load = another.load + this.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(83);
+        Battery second = new Battery(17);
+        first.exchange(second);
+        System.out.println("first: " + first.load + " second: " + second.load);
+        second.exchange(first);
+        System.out.println("first: " + first.load + " second: " + second.load);
+    }
+}
