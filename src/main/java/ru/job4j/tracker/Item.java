f99073c1db83d47fdaab207741747cbb8e46ae6b
heeykat <heeykat@yandex.ru>
2021-02-16 21:30:03 +0300

    7. Date. Отображение даты. [#438136]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,10 +1,14 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private String name;
     private int id;
+    private LocalDateTime created;
 
     public Item() {
+        this.created = LocalDateTime.now();
     }
 
     public Item(int id){
@@ -22,5 +26,9 @@ public class Item {
 
     public static void main(String[] args) {
         Item item = new Item();
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
