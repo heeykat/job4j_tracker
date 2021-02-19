a8bdd495e9e76c25c64fa1b435f04a1ae287a7f3
heeykat <heeykat@yandex.ru>
2021-02-19 10:37:28 +0300

    9. toString [#438148]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,5 +7,10 @@ public class StartUI {
         tracker.add(item);
         int id = item.getId();
         System.out.println(tracker.findById(id));
+
+        Item item1 = new Item();
+        item1.setId(5);
+        item1.setName("Имя");
+        System.out.println(item1);
     }
 }
