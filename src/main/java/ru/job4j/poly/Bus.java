3360657c3eaee53e628a6a15695cfa419ee3daa1
heeykat <heeykat@yandex.ru>
2021-02-20 16:57:33 +0300

    3. Интерфейс Input [#438071]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void ride() {
+
+    }
+
+    @Override
+    public void passengers(int count) {
+
+    }
+
+    @Override
+    public int fill(int volume) {
+        return 0;
+    }
+}
